0c1c2ee0dcc713861a7a14159ab5d18ecb633491
Ritik Khadgi <kritik18@tbc.edu.np>
2022-04-05 20:36:15 +0545

    Activities and Intent Homework

---
+++ app/src/main/java/com/example/activitiesandintenthomework/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.activitiesandintenthomework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount=0;
+    private TextView sendCount;
+    private TextView mShowCount;
+
+    public static final String EXTRA_MESSAGE="com.example.android.hellotoast.extra.Message";
+    public static final int TEXT_REQUEST =1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount=(TextView)findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+//        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+//        toast.show();
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(EXTRA_MESSAGE,mCount);
+        startActivity(intent);
+
+
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if(mShowCount!= null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+}
